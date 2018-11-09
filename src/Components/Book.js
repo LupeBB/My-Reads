b40e6cb5e7c52e4b1c35d94109cf2eadec28597e
LupeBB <40974786+LupeBB@users.noreply.github.com>
2018-11-09 00:19:19 -0800

    Update Book.js

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -8,49 +8,30 @@ class Book extends React.Component {
   render() {
     return (
       <li>
-        <div className="book">
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 125,
-                height: 190,
-                // Creates book thumbnail url
-                backgroundImage: `url(${
-                  this.props.book.imageLinks
-                    ? this.props.book.imageLinks.thumbnail
-                    : "Default url or none"
-                },)`
-              }}
-            />
-            <div />
-            <div className="book-shelf-changer">
-              <select
-                value={this.props.book.shelf || "none"}
-                onChange={e => {
-                  this.props.updateBook(this.props.book, e.target.value);
-                }}
-              >
-                <option value="move" disabled>
-                  {" "}
-                  Move to...
-                </option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">
-          {" "}
-          {(this.props.book.authors && this.props.book.authors[0]) ||
-            "No author..."}{" "}
-        </div>
-      </li>
-    );
-  }
+      <div className="book">
+      <div className="book-top">
+      <div className="book-cover"
+      style={{ 
+        width: 125, 
+        height: 190,
+        backgroundImage: // Creates book thumbnail url
+        `url(${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail: "Default url or none"},)`}}/>
+     <div/>
+  <div className="book-shelf-changer">
+  <select value={this.props.book.shelf || "none"} onChange={e => {this.props.updateBook(this.props.book, e.target.value)} }>
+     <option value="move" disabled> Move to...</option>
+     <option value="currentlyReading">Currently Reading</option>
+     <option value="wantToRead">Want to Read</option>
+     <option value="read">Read</option>
+     <option value="none">None</option>
+    </select>
+    </div>
+  </div>
+</div>
+    <div className="book-title">{this.props.book.title}</div>
+    <div className="book-authors"> {(this.props.book.authors && this.props.book.authors[0]) || "No author..."} </div>
+</li>
+  );
+ }
 }
 export default Book;
