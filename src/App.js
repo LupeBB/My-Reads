293b115b930fd2e81b3da050c6e5eb6e92985d8e
LupeBB <40974786+LupeBB@users.noreply.github.com>
2018-11-09 23:31:02 -0800

    Update App.js
    Per reviewers suggestion, changed App.js to import last in order

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import "./App.css";
-
 import { Route } from "react-router-dom";
 
 import MainShelf from "./Components/pages/MainShelf";
 
 import SearchScreen from "./Components/pages/SearchScreen";
+
+import "./App.css";
 
 class BooksApp extends React.Component {
   render() {
